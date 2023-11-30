de97e28208becf158adc1edc725e640db5f6a43d
inubayuaji <inu.develop@gmail.com>
2023-11-30 08:38:50 +0700

    cart link

--- resources/views/web/layouts/base.blade.php
+++ resources/views/web/layouts/base.blade.php
@@ -73,7 +73,7 @@
                     </ul>
                 </div>
                 <div class="navbar align-self-center d-flex">
-                    <a class="nav-icon position-relative text-decoration-none" href="#">
+                    <a class="nav-icon position-relative text-decoration-none" href="{{ route('web.cart') }}">
                         <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
                         @if(Auth::guard('customer')->check())
                         <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">
