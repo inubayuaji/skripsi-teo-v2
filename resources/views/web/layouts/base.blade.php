6f02156605e73ed559e6f2a19f81c462be45d80b
inubayuaji <inu.develop@gmail.com>
2024-09-06 08:19:30 +0700

    fix footer info

--- resources/views/web/layouts/base.blade.php
+++ resources/views/web/layouts/base.blade.php
@@ -119,19 +119,15 @@
             <div class="row">
 
                 <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Zay Shop</h2>
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Toko Tani</h2>
                     <ul class="list-unstyled text-light footer-link-list">
                         <li>
                             <i class="fas fa-map-marker-alt fa-fw"></i>
-                            123 Consectetur at ligula 10660
+                            Jl. Raya Tayu - Pati
                         </li>
                         <li>
                             <i class="fa fa-phone fa-fw"></i>
-                            <a class="text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope fa-fw"></i>
-                            <a class="text-decoration-none" href="mailto:info@company.com">info@company.com</a>
+                            <a class="text-decoration-none" href="tel:085290038847">085290038847</a>
                         </li>
                     </ul>
                 </div>
